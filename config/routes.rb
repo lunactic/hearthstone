9a450f1fe12d2cf95d2a2bf4dbc80bdd95d662a7
Marcel Würsch <marcel.wuersch@students.unibe.ch>
2013-10-07 10:15:08 +0200

    added card route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Hearthstone::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root to:'welcome#index'
 
+  resources :cards
+
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
