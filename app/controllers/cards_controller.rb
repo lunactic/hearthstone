432a1de27aedb6bf8e2455c18dee2e439885584f
Marcel Würsch <marcel.wuersch@students.unibe.ch>
2013-10-08 12:32:28 +0200

    First Card Controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,19 @@ class CardsController < ApplicationController
   def new
 
   end
+
+  def create
+    @post = Card.new(post_params)
+    @post.save
+    redirect_to @post
+  end
+
+  def index
+    @post = Card.all
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:name, :class, :cost, :attack, :health, :race, :rarity, :type)
+    end
 end
