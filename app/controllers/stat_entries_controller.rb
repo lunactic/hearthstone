2016909ac2d09c7fc18fd7719bdb348904b67ea6
Steve Aschwanden <steve.aschwanden@students.unibe.ch>
2013-10-22 20:07:08 +0200

    Added views and controller for the StatEntry model

--- app/controllers/stat_entries_controller.rb
+++ app/controllers/stat_entries_controller.rb
@@ -1,8 +1,25 @@
 class StatEntriesController < ApplicationController
+
 	def new
+		@stat_entry = StatEntry.new
+	end
+
+	def create
+		@stat_entry = StatEntry.new(params[:stat_entry].permit(:hero, :opp_hero, :game_mode, :result, :first))
+ 
+		if @stat_entry.save
+			redirect_to @stat_entry
+		else
+			render 'new'
+		end
 	end
 
 	def edit
+		@stat_entry = StatEntry.find(params[:id])
+	end
+
+	def show
+		@stat_entry = StatEntry.find(params[:id])
 	end
 
 	def index
@@ -14,5 +31,15 @@ class StatEntriesController < ApplicationController
 	  @stat_entry.destroy
  
 	  redirect_to stat_entries_path
+	end
+
+	def update
+		@stat_entry = StatEntry.find(params[:id])
+	 
+		if @stat_entry.update(params[:stat_entry].permit(:hero, :opp_hero, :game_mode, :result, :first))
+		  redirect_to @stat_entry
+		else
+		  render 'edit'
+		end
 	end
 end
