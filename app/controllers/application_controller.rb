3b507b4add37ba557513e534571b2db9800112bf
Michael <michu.baertschi@students.unibe.ch>
2013-11-04 12:17:41 +0100

    added realname to users (not mandatory)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,11 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-	  devise_parameter_sanitizer.for(:sign_up) << :username
+	  devise_parameter_sanitizer.for(:sign_up) do |u|
+		  u.permit :username, :realname, :email, :password, :password_confirmation
+	  end
 	  devise_parameter_sanitizer.for(:account_update) do |u|
-		  u.permit :username, :email, :password, :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar
+		  u.permit :username, :realname, :email, :password, :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar
 	  end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131031135736) do
+ActiveRecord::Schema.define(version: 20131104105705) do
 
   create_table "cards", force: true do |t|
     t.string   "name"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20131031135736) do
     t.datetime "updated_at"
     t.string   "username"
     t.string   "avatar"
+    t.string   "realname"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
